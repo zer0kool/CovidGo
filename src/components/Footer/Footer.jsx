c1a69f139ff7dc13f8f74be26996bcad675b35d4
zer0kool <zer0kool@users.noreply.github.com>
2020-03-31 22:46:09 -0700

    updated footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,32 @@ import "./Footer.css";
 
 export default class Footer extends Component {
   render() {
-    return <div className="Footer">Footer COMP</div>;
+    return (
+        <div className="page-footer blue-grey darken-4">
+          <div className="container">
+            <div className="row">
+              <div className="col l6 s12">
+                <h5 className="white-text">CovidGo</h5>
+                <p className="grey-text text-lighten-4">Built with hope to bring more awareness to communities and make them realize that staying at home can stop this pandemic.</p>
+              </div>
+              <div className="col l4 offset-l2 s12">
+                <h5 className="white-text">Resources</h5>
+                <ul>
+                  <li><a className="grey-text text-lighten-3" href="#!">React</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">MaterializeCSS</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Github</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">HostingProvider</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="footer-copyright">
+            <div className="container">
+            © 2020 CovidGo
+            <a className="grey-text text-lighten-4 right" href="#!">#StayHome</a>
+            </div>
+          </div>
+        </div>
+    );
   }
 }
