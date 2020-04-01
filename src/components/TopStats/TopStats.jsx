70d1d2526a67ef105d314968bef133cd72e4427c
zer0kool <zer0kool@users.noreply.github.com>
2020-04-01 09:03:31 -0700

    Updated the top stats layout

--- src/components/TopStats/TopStats.jsx
+++ src/components/TopStats/TopStats.jsx
@@ -6,11 +6,27 @@ import "./TopStats.css";
 export default class TopStats extends Component {
   render() {
     return (
-        <div className="TopStats">
-        <div className="card">Infectiopns</div>
-        <div className="card">Deaths</div>
-        <div className="card">Recoveries</div>
-        <div className="card">Critical</div>
+        <div className="top-container">
+            <div className="cardx">
+                <span>Infections</span>
+                <h3>858,920</h3>
+                <p>1688 Today</p>
+            </div>
+            <div className="cardx">
+                <span>Deaths</span>
+                <h3>42,322</h3>
+                <p>42 Today</p>
+            </div>
+            <div className="cardx">
+                <span>Recoveries</span>
+                <h3>178.100</h3>
+                <p>639.398 remaining</p>
+            </div>
+            <div className="cardx">
+                <span>Critical</span>
+                <h3>32,898</h3>
+                <p>67,177.4 per million</p>
+            </div>
         </div>
     );
   }
