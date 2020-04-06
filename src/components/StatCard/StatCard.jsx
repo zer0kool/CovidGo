b08999c27dc5334fd3434e2072192b1c0525be2e
zer0kool <zer0kool@users.noreply.github.com>
2020-04-05 19:03:27 -0700

    updated icon

--- src/components/StatCard/StatCard.jsx
+++ src/components/StatCard/StatCard.jsx
@@ -9,9 +9,9 @@ export default class TopStats extends Component {
     render() {
         return (
             <div className="cardx">
-                <span>{this.props.label}</span>
+                <span><i className="material-icons">{this.props.icon}</i>{this.props.label}</span>
                 <h3>{this.props.totalStats}</h3>
-                <p>{this.props.todayStats}</p>
+                <p><i className="tiny material-icons">{this.props.icon2}</i>{this.props.todayStats}</p>
             </div>
         );
     }
