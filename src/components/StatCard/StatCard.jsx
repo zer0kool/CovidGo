44637d10eed4111b9eeecc9d35d01e7d55ff74d2
Ulises E. Cervantes Torres <ulises.cerv@hotmail.com>
2020-04-07 15:58:31 -0500

    Adding SCSS

--- src/components/StatCard/StatCard.jsx
+++ src/components/StatCard/StatCard.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 
-
 //CSS
-
+import "./StatCard.css";
 
 export default class TopStats extends Component {
-
     render() {
         return (
             <div className="cardx">
-                <span><i className="material-icons">{this.props.icon}</i>{this.props.label}</span>
-                <h3>{this.props.totalStats}</h3>
-                <p><i className="tiny material-icons">{this.props.icon2}</i>{this.props.todayStats}</p>
+                <div className="cardxContainer">
+                    <span>
+                        <i className="material-icons">{this.props.icon}</i>
+                        {this.props.label}
+                    </span>
+                    <h3>{this.props.totalStats}</h3>
+                    <p>
+                        <i className="tiny material-icons">
+                            {this.props.icon2}
+                        </i>
+                        {this.props.todayStats}
+                    </p>
+                </div>
             </div>
         );
     }
