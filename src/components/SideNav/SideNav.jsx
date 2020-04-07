1d86a6fb10c3e004f64d2c2f760a0dbc83ece571
zer0kool <zer0kool@users.noreply.github.com>
2020-04-07 16:36:01 -0700

    Fixed side navigation  panel

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,15 +1,76 @@
 import React, { Component } from "react";
 
 import "./sideNav.css";
+
 export default class SideNav extends Component {
   render() {
     return (
-        <div>
-            <ul id="slide-out" className="sidenav">
-              <li><a href="#!">First Sidebar Link</a></li>
-              <li><a href="#!">Second Sidebar Link</a></li>
+        <div id="slide-out" className="sidenav">
+           <div className="navtop">
+             <a href="#!" ClassName="right waves-effect waves-light navbtn sidenav-close">close</a>
+             <h3 className="menutitle">Menu</h3>
+           </div>
+            <ul className="navList">
+              <li className="item">
+                  <a href="#!" className="menuitem"><i className="material-icons white-text">assignment_ind</i>Learn about Symptoms</a>
+              </li>
+              <li className="item">
+                  <a href="#!" className="menuitem"><i className="material-icons white-text">cloud</i>Tacking Precautions</a>
+              </li>
+              <li className="item">
+                  <a href="#!" className="menuitem"><i className="material-icons white-text">cloud</i>What is Covid-19</a>
+            </li>
             </ul>
-            <a href="#" data-target="slide-out" className="sidenav-trigger show-on-large"><i className="material-icons">menu</i></a>
+
+            <div className="row">
+              <div ClassName="col s12 m4">
+                <div ClassName="card horizontal">
+                  <div ClassName="card-image">
+                    <img src="./covidCell.jpg" />
+                  </div>
+                  <div ClassName="card-stacked">
+                    <div ClassName="card-content">
+                      <p>I am a very simple card. I am good at containing small bits of information.</p>
+                    </div>
+                    <div ClassName="card-action">
+                      <a href="#">This is a link</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+             <div ClassName="col s12 m4">
+                <div ClassName="card horizontal">
+                  <div ClassName="card-image">
+                    <img src="../microCovid.png" />
+                  </div>
+                  <div ClassName="card-stacked">
+                    <div ClassName="card-content">
+                      <p>I am a very simple card. I am good at containing small bits of information.</p>
+                    </div>
+                    <div ClassName="card-action">
+                      <a href="#">This is a link</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <ul ClassName="collapsible">
+            <li ClassName="item">
+              <div ClassName="collapsible-header menuitem blue-grey darken-4"><i ClassName="material-icons">filter_drama</i>About Us</div>
+              <div ClassName="collapsible-body blue-grey darken-4"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+            <li ClassName="item">
+              <div ClassName="collapsible-header menuitem blue-grey darken-4"><i ClassName="material-icons">place</i>About CovidGo</div>
+              <div ClassName="collapsible-body blue-grey darken-4"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+            <li ClassName="item">
+              <div ClassName="collapsible-header menuitem blue-grey darken-4"><i ClassName="material-icons">whatshot</i>Privacy</div>
+              <div ClassName="collapsible-body blue-grey darken-4"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+          </ul>
+
         </div>
     );
   }
