a2f38fc2304ebd1264c38c3e9e785884f9761669
Cervantes Torres Ulises Emmanuel (RBEI/EBS3.2-MX) <ceu1ga@bosch.com>
2020-04-07 09:45:08 -0500

    Theme changed in DistributionChart comp

--- src/components/DistributionChart/DistributionChart.jsx
+++ src/components/DistributionChart/DistributionChart.jsx
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import am4themes_animated from "@amcharts/amcharts4/themes/material";
 
 import "./DistributionChart.css";
 
@@ -111,7 +111,7 @@ export default class DistributionChart extends Component {
         });
 
         // Add cursor
-        chart.cursor = new am4charts.RadarCursor();
+        // chart.cursor = new am4charts.RadarCursor();
 
         this.chart = chart;
     }
