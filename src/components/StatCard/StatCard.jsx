aed223024d7f2e92000721dec1de0515f588c4ff
zer0kool <zer0kool@users.noreply.github.com>
2020-04-07 23:19:01 -0700

    Updated cards to reveal graphs on click

--- src/components/StatCard/StatCard.jsx
+++ src/components/StatCard/StatCard.jsx
@@ -6,19 +6,23 @@ import "./StatCard.css";
 export default class TopStats extends Component {
     render() {
         return (
-            <div className="cardx">
-                <div className="cardxContainer">
-                    <span>
+            <div className="card activator">
+                <div className="cardxContainer card-content">
+                    <span className="activator">
                         <i className="material-icons">{this.props.icon}</i>
                         {this.props.label}
                     </span>
-                    <h3>{this.props.totalStats}</h3>
-                    <p>
+                    <h3 className="activator">{this.props.totalStats}</h3>
+                    <p className="activator">
                         <i className="tiny material-icons">
                             {this.props.icon2}
                         </i>
                         {this.props.todayStats}
                     </p>
+                </div>
+                <div className="card-reveal">
+                      <span className="card-title grey-text text-darken-4">{this.props.label}<i className="material-icons right">close</i></span>
+                      <p>GRAPH DATA HERE</p>
                 </div>
             </div>
         );
