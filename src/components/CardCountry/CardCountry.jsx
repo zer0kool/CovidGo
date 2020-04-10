a02b97e4af18e2004c898aabd4a6cacd5d4fce56
zer0kool <zer0kool@users.noreply.github.com>
2020-04-09 22:14:32 -0700

    Merging ulises commits to staging

--- src/components/CardCountry/CardCountry.jsx
+++ src/components/CardCountry/CardCountry.jsx
@@ -4,6 +4,10 @@ import React, { Component } from "react";
 import "./CardCountry.css";
 
 export default class CardCountry extends Component {
+
+    formatNumber = (num) => {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
     render() {
         const {
             country,
@@ -24,23 +28,30 @@ export default class CardCountry extends Component {
                             <div className="data_country">
                                 <span className="name_country">{country}</span>
                                 <div className="cases_data_country">
-                                    <span className="cases_country blue-text">
-                                        {cases} Cases
+                                    <span className="white-text title">Total</span>
+                                    <span className="cases_country blue-text data">
+                                        {this.formatNumber(cases)} Cases
                                     </span>
-                                    <span>&</span>
-                                    <span className="death_country red-text text-accent-2">
-                                        {deaths} Deaths
+                                    <span className="death_country red-text text-accent-2 data">
+                                        {this.formatNumber(deaths)} Deaths
                                     </span>
                                 </div>
                             </div>
                         </div>
                         <div className="right">
                             <div className="today_data_country">
-                                <span className="cases_country blue-text">
-                                    + {todayCases} Cases
+                                <span className="white-text title">Today</span>
+                                <span className="today_data_country_number cases_country blue-text data">
+                                    <i className="tiny material-icons">{"group_add"}</i>
+                                    <span>
+                                        {this.formatNumber(todayCases)} Cases
+                                    </span>
                                 </span>
-                                <span className="death_country red-text  text-accent-2">
-                                    + {todayDeaths} Deaths
+                                <span className="today_data_country_number death_country red-text  text-accent-2 data">
+                                    <i className="tiny material-icons">{"group_add"}</i>
+                                    <span>
+                                        {this.formatNumber(todayDeaths)} Deaths
+                                    </span>
                                 </span>
                             </div>
                         </div>
