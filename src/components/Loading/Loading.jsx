2faa8ba0a7b543da10c67034ea9e96beef6db44a
Ulises Cervantes <ulises.cerv@hotmail.com>
2020-04-17 16:26:43 -0500

    Fixed issue with MArgin and TopStats COmponent

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,16 +4,16 @@ import './Loading.css'
 export default class Loading extends Component {
     render() {
         return <div className="loader">
-            <div class="preloader-wrapper big active">
-                <div class="spinner-layer spinner-blue">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
+            <div className="preloader-wrapper big active">
+                <div className="spinner-layer spinner-blue">
+                    <div className="circle-clipper left">
+                        <div className="circle"></div>
                     </div>
-                    <div class="gap-patch">
-                        <div class="circle"></div>
+                    <div className="gap-patch">
+                        <div className="circle"></div>
                     </div>
-                    <div class="circle-clipper right">
-                        <div class="circle"></div>
+                    <div className="circle-clipper right">
+                        <div className="circle"></div>
                     </div>
                 </div>
             </div>
