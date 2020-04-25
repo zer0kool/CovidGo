4d4875c641b2b10df3c5b00bba02185b5ad64bfd
zer0kool <zer0kool@users.noreply.github.com>
2020-04-25 13:20:02 -0700

    component fetch and error best practices

--- src/components/Wavook/Wavook.jsx
+++ src/components/Wavook/Wavook.jsx
@@ -18,48 +18,47 @@ export default class Wavook extends Component {
     }
 
     render() {
-        const { wavookData, isLoading } = this.state;
+        const { wavookData, isLoading, error } = this.state;
 
-        if ( isLoading ){
-            return <p> Loading Data...</p>;
-        }
+        if ( !wavookData ){ return <p className="white-text"> No Data...</p>;}
+        if ( isLoading ){ return <p className="white-text"> Loading Data...</p>;}
+        if ( error ){ return <p className="white-text"> {error.message} </p>;}
 
         return (
             <div id="Wavook">
                  {wavookData.map( post =>
                  <div key={post.objectID}>
-                     <td>{post.title}</td>
-                     <td>{post.url}</td>
-                     <td>{post.views}</td>
+                     <p>{post.title}</p>
+                     <p>{post.url}</p>
+                     <p>{post.views}</p>
                  </div>)}
             </div>
         );
     }
 
-    componentDidMount() {
+   async componentDidMount() {
         this.setState({ isLoading: true })
+        let self = this
         var data = [];
         fetch(proxy+Endpoint)
-            .then(response => response.text())
+            .then(response => response.text()) // Aqui podemos agregar mas error handling -alex.
             .then(function (html){
-                let parser = new DOMParser();
-                let wavook = parser.parseFromString(html, 'text/html');
-                let posts = wavook.querySelectorAll(".blogpost");
-
-                console.log(data)
-                posts.forEach( scrape => {
-                    let post = { title: "", url: "", author:"", avatar:"", views:"" };
-                    post.title = scrape.children[0].children[0].children[0].text;
-                    post.url = scrape.children[0].children[0].children[0].href;
-                    post.author = scrape.children[1].children[0].innerText;
-                    post.avatar = scrape.children[1].children[0].firstElementChild.currentSrc;
-                    post.views = scrape.children[1].children[1].children[1].textContent;
-                    data.push(post);
-                })
-
-                this.setState({ wavookData: data, isLoading: false })
+                try{
+                    let parser = new DOMParser();
+                    let wavook = parser.parseFromString(html, 'text/html');
+                    let posts = wavook.querySelectorAll(".blogpost");
+                    posts.forEach( scrape => {
+                        let post = { title: "", url: "", author:"", avatar:"", views:"" };
+                        post.title = scrape.children[0].children[0].children[0].text;
+                        post.url = scrape.children[0].children[0].children[0].href;
+                        post.author = scrape.children[1].children[0].innerText;
+                        post.avatar = scrape.children[1].children[0].firstElementChild.currentSrc;
+                        post.views = scrape.children[1].children[1].children[1].textContent;
+                        data.push(post);
+                    })
+                } catch (error) {console.log(`Error inside the parsing function: ${error}`)}
+            self.setState({ wavookData: data, isLoading: false })
 
         }).catch( error => this.setState({ error, isLoading: false }))
-
     }
 }
