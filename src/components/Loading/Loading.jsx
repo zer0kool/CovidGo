249aff5ebcbd1106d0d3823b3540e92e17d91cae
zer0kool <zer0kool@users.noreply.github.com>
2020-05-04 12:47:13 -0700

    Fixed classname issues on dom rendering

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,52 +4,52 @@ import './Loading.css'
 export default class Loading extends Component {
     render() {
         return <div className="loader">
-            <div class="preloader-wrapper big active">
-                <div class="spinner-layer spinner-blue">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
+            <div className="preloader-wrapper big active">
+                <div className="spinner-layer spinner-blue">
+                    <div className="circle-clipper left">
+                        <div className="circle"></div>
                     </div>
-                    <div class="gap-patch">
-                        <div class="circle"></div>
+                    <div className="gap-patch">
+                        <div className="circle"></div>
                     </div>
-                    <div class="circle-clipper right">
-                        <div class="circle"></div>
-                    </div>
-                </div>
-
-                <div class="spinner-layer spinner-red">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
-                    </div>
-                    <div class="gap-patch">
-                        <div class="circle"></div>
-                    </div>
-                    <div class="circle-clipper right">
-                        <div class="circle"></div>
+                    <div className="circle-clipper right">
+                        <div className="circle"></div>
                     </div>
                 </div>
 
-                <div class="spinner-layer spinner-yellow">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
+                <div className="spinner-layer spinner-red">
+                    <div className="circle-clipper left">
+                        <div className="circle"></div>
                     </div>
-                    <div class="gap-patch">
-                        <div class="circle"></div>
+                    <div className="gap-patch">
+                        <div className="circle"></div>
                     </div>
-                    <div class="circle-clipper right">
-                        <div class="circle"></div>
+                    <div className="circle-clipper right">
+                        <div className="circle"></div>
                     </div>
                 </div>
 
-                <div class="spinner-layer spinner-green">
-                    <div class="circle-clipper left">
-                        <div class="circle"></div>
+                <div className="spinner-layer spinner-yellow">
+                    <div className="circle-clipper left">
+                        <div className="circle"></div>
                     </div>
-                    <div class="gap-patch">
-                        <div class="circle"></div>
+                    <div className="gap-patch">
+                        <div className="circle"></div>
                     </div>
-                    <div class="circle-clipper right">
-                        <div class="circle"></div>
+                    <div className="circle-clipper right">
+                        <div className="circle"></div>
+                    </div>
+                </div>
+
+                <div className="spinner-layer spinner-green">
+                    <div className="circle-clipper left">
+                        <div className="circle"></div>
+                    </div>
+                    <div className="gap-patch">
+                        <div className="circle"></div>
+                    </div>
+                    <div className="circle-clipper right">
+                        <div className="circle"></div>
                     </div>
                 </div>
             </div>
